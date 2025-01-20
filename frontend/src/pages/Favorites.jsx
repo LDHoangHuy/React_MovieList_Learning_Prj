a9968910e820a1bbd4a4cad5b2f7774b73803288
Le Dang Hoang Huy <ledanghoanghuy2004@gmail.com>
2025-01-20 16:11:38 +0700

    chore: reformat code

--- frontend/src/pages/Favorites.jsx
+++ frontend/src/pages/Favorites.jsx
@@ -1,8 +1,10 @@
 function Favorites() {
-    return <div className="favorites-empty">
-        <h2>No Favorite Movies Yet</h2>
-        <p>Start adding movies to your favorites and they will appear here</p>
+  return (
+    <div className="favorites-empty">
+      <h2>No Favorite Movies Yet</h2>
+      <p>Start adding movies to your favorites and they will appear here</p>
     </div>
+  );
 }
 
 export default Favorites;
